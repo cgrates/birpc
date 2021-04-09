0f59ab59c82574aa34d8bb764e18b7b351bd6657
Trial97 <alexandru.tripon@itsyscom.com>
2021-04-09 09:36:32 +0300

    updated bench

--- bench_test.go
+++ bench_test.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	contextg "context"
+	"log"
 	"net"
 	"testing"
 
@@ -9,6 +10,34 @@ import (
 	"github.com/cgrates/rpc/context"
 	"github.com/cgrates/rpc/rpcc"
 )
+
+func BenchmarkBirpcInArgsOverRPC(b *testing.B) {
+	newServer := birpc.NewBirpcServer()
+	newServer.Register(new(birpc.Airth3))
+	l, addr := listenTCP()
+	log.Println("NewServer test RPC server listening on", newServerAddr)
+	go newServer.Accept(l)
+
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	client := birpc.NewBirpcClient(c)
+	defer client.Close()
+	client.Register(new(birpc.Airth3))
+	ctx := contextg.Background()
+
+	// Synchronous calls
+	args := &birpc.Args3{7, 8}
+	reply := new(birpc.Reply3)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := client.Call(ctx, "Airth3.Add", args, reply); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
 
 func BenchmarkBirpcInArgs(b *testing.B) {
 	newServer := birpc.NewBirpcServer()
@@ -33,6 +62,34 @@ func BenchmarkBirpcInArgs(b *testing.B) {
 	}
 }
 
+func BenchmarkBirpcInContextOverRPC(b *testing.B) {
+	newServer := NewBirpcServer()
+	newServer.Register(new(Airth2))
+	l, addr := listenTCP()
+	log.Println("NewServer test RPC server listening on", newServerAddr)
+	go newServer.Accept(l)
+
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	client := NewBirpcClient(c)
+	defer client.Close()
+	client.Register(new(Airth2))
+	ctx := context.Background()
+
+	// Synchronous calls
+	args := &Args2{7, 8}
+	reply := new(Reply2)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := client.Call(ctx, "Airth2.Add", args, reply); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
 func BenchmarkBirpcInContext(b *testing.B) {
 	newServer := NewBirpcServer()
 	newServer.Register(new(Airth2))
@@ -47,6 +104,34 @@ func BenchmarkBirpcInContext(b *testing.B) {
 	// Synchronous calls
 	args := &Args2{7, 8}
 	reply := new(Reply2)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := client.Call(ctx, "Airth2.Add", args, reply); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkBirpcInContextReflectOverRPC(b *testing.B) {
+	newServer := rpcc.NewBirpcServer()
+	newServer.Register(new(rpcc.Airth2))
+	l, addr := listenTCP()
+	log.Println("NewServer test RPC server listening on", newServerAddr)
+	go newServer.Accept(l)
+
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	client := rpcc.NewBirpcClient(c)
+	defer client.Close()
+	client.Register(new(rpcc.Airth2))
+	ctx := context.Background()
+
+	// Synchronous calls
+	args := &rpcc.Args2{7, 8}
+	reply := new(rpcc.Reply2)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
